1c3b9f20df82fc7729f310bd8815997cf3c7c103
Hannes Soosaar <135945234+Hannes-Soosaar@users.noreply.github.com>
2023-08-17 09:08:26 +0300

    Add files via upload
    Updated material from out daily work.

---
+++ notes/JS_greeting.js
@@ -0,0 +1,11 @@
+const nimi = "Kaspar";
+let vanus = 29;
+
+function terviatKasutajat(eesnimi, kasutajaVanus) {
+ 
+const tervitus = ("Tere, "+  eesnimi +"! Teie vanus on " + kasutajaVanus + " aasta.");
+
+ console.log(tervitus);
+}
+
+terviatKasutajat(nimi,vanus)
